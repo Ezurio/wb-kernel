16fe8fb39de7ea1d809f211106acc640c833e5e2
Praveen Babu C <pucn@cypress.com>
2018-01-09 11:33:10 +0530

    non-upstream: support DS1 exit firmware re-download
    In deep sleep mode (DS1) ARM is off and once exit trigger comes than
    mailbox Interrupt comes to host and whole reinitiation should be done
    in the ARM to start TX/RX.
    
    Also fix below issus for DS1 exit:
    1. Sent Tx Control frame only after firmware redownload complete (check
    F2 Ready before sending Tx Control frame to Firmware)
    2. intermittent High DS1 TX Exit latency time (almost 3sec) ==> This is
    fixed by skipping host Mailbox interrupt Multiple times (ulp state
    mechanism)
    3. RX GlOM save/restore in Firmware
    4. Add ULP event enable & event_msgs_ext iovar configuration in FMAC
    5. Add ULP_EVENT_RECV state machine for sbwad support
    6. Support 2 Byte Shared memory read for DS1 Exit HUDI implementation
    
    Signed-off-by: Praveen Babu C <pucn@cypress.com>
    Signed-off-by: Naveen Gupta <nagu@cypress.com>
    [Merge from 4.14.77 to 5.4.18; set BRCMF_SDIO_MAX_ACCESS_ERRORS to 20]
    Signed-off-by: Chi-hsien Lin <chi-hsien.lin@cypress.com>

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/fweh.h
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/fweh.h
@@ -96,6 +96,7 @@ struct brcmf_cfg80211_info;
 	BRCMF_ENUM_DEF(TDLS_PEER_EVENT, 92) \
 	BRCMF_ENUM_DEF(PHY_TEMP, 111) \
 	BRCMF_ENUM_DEF(BCMC_CREDIT_SUPPORT, 127) \
+	BRCMF_ENUM_DEF(ULP, 146) \
 	BRCMF_ABSTRACT_ENUM_DEF(EXT_AUTH_REQ, 0) \
 	BRCMF_ABSTRACT_ENUM_DEF(EXT_AUTH_FRAME_RX, 1) \
 	BRCMF_ABSTRACT_ENUM_DEF(MGMT_FRAME_TXSTATUS, 2) \
@@ -107,6 +108,11 @@ struct brcmf_cfg80211_info;
 /* firmware event codes sent by the dongle */
 enum brcmf_fweh_event_code {
 	BRCMF_FWEH_EVENT_ENUM_DEFLIST
+	/* this determines event mask length which must match
+	 * minimum length check in device firmware so it is
+	 * hard-coded here.
+	 */
+	BRCMF_E_LAST = 147
 };
 #undef BRCMF_ENUM_DEF
 
@@ -283,6 +289,28 @@ struct brcmf_if_event {
 	u8 flags;
 	u8 bsscfgidx;
 	u8 role;
+};
+
+enum event_msgs_ext_command {
+	EVENTMSGS_NONE		=	0,
+	EVENTMSGS_SET_BIT	=	1,
+	EVENTMSGS_RESET_BIT	=	2,
+	EVENTMSGS_SET_MASK	=	3
+};
+
+#define EVENTMSGS_VER 1
+#define EVENTMSGS_EXT_STRUCT_SIZE	offsetof(struct eventmsgs_ext, mask[0])
+
+/* len-	for SET it would be mask size from the application to the firmware */
+/*		for GET it would be actual firmware mask size */
+/* maxgetsize -	is only used for GET. indicate max mask size that the */
+/*				application can read from the firmware */
+struct eventmsgs_ext {
+	u8	ver;
+	u8	command;
+	u8	len;
+	u8	maxgetsize;
+	u8	mask[1];
 };
 
 typedef int (*brcmf_fweh_handler_t)(struct brcmf_if *ifp,
--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/usb.c
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/usb.c
@@ -1216,7 +1216,7 @@ static void brcmf_usb_probe_phase2(struct device *dev, int ret,
 	}
 
 	/* Attach to the common driver interface */
-	ret = brcmf_attach(devinfo->dev);
+	ret = brcmf_attach(devinfo->dev, true);
 	if (ret)
 		goto error;
 
@@ -1303,7 +1303,7 @@ static int brcmf_usb_probe_cb(struct brcmf_usbdev_info *devinfo,
 				goto fail;
 		}
 
-		ret = brcmf_attach(devinfo->dev);
+		ret = brcmf_attach(devinfo->dev, true);
 		if (ret)
 			goto fail;
 
