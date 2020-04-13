39cea683f2b83ce6f9708d3e44a3dc1d0f14bee4
Ting-Ying Li <tingying.li@cypress.com>
2020-04-13 02:39:44 -0500

    brcmfmac: don't allow arp/nd offload to be enabled if ap mode exists
    Add a condition to determine whether arp/nd offload enabling
    request is allowed. If there is any interface acts as ap
    mode and is operating, then reject the request of arp oflload
    enabling from cfg80211.
    
    Signed-off-by: Ting-Ying Li <tingying.li@cypress.com>

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/cfg80211.h
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/cfg80211.h
@@ -488,5 +488,6 @@ void brcmf_abort_scanning(struct brcmf_cfg80211_info *cfg);
 void brcmf_cfg80211_free_netdev(struct net_device *ndev);
 
 int brcmf_set_wsec(struct brcmf_if *ifp, const u8 *key, u16 key_len, u16 flags);
+bool brcmf_is_apmode_operating(struct wiphy *wiphy);
 
 #endif /* BRCMFMAC_CFG80211_H */
