120d9dc0c265c70c46b86407c0ee8887b3a7eca9
Double Lo <double.lo@cypress.com>
2020-07-06 04:10:49 -0500

    brcmfmac: support 43439 Cypress Vendor and Device ID
    To add 43439 Cypress Vendor and Device ID:
    43439 Cypress VID : 0x04b4
    43439 Cypress DID : 0xbd3d
    
    Signed-off-by: Double Lo <double.lo@cypress.com>

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/bcmsdh.c
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/bcmsdh.c
@@ -974,6 +974,7 @@ out:
 		.driver_data = BRCMF_FWVENDOR_ ## fw_vend \
 	}
 
+
 /* devices we support, null terminated */
 static const struct sdio_device_id brcmf_sdmmc_ids[] = {
 	BRCMF_SDIO_DEVICE(SDIO_DEVICE_ID_BROADCOM_43143, WCC),
@@ -999,7 +1000,7 @@ static const struct sdio_device_id brcmf_sdmmc_ids[] = {
 	BRCMF_SDIO_DEVICE(SDIO_DEVICE_ID_BROADCOM_CYPRESS_4373, CYW),
 	BRCMF_SDIO_DEVICE(SDIO_DEVICE_ID_BROADCOM_CYPRESS_43012, CYW),
 	BRCMF_SDIO_DEVICE(SDIO_DEVICE_ID_BROADCOM_CYPRESS_89359, CYW),
-	CYW_SDIO_DEVICE(SDIO_DEVICE_ID_BROADCOM_CYPRESS_43439, CYW),
+	CYW_SDIO_DEVICE(SDIO_DEVICE_ID_CYPRESS_43439, CYW),
 	{ /* end: all zeroes */ }
 };
 MODULE_DEVICE_TABLE(sdio, brcmf_sdmmc_ids);
