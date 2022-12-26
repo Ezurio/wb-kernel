5b072114a56fa24ca8a74315cdfd7c19faf46ed7
Owen Huang <Owen.Huang@infineon.com>
2022-12-25 20:12:03 -0600

    non-upstream: btsdio: enable export api for F3 receiving buffer
    1. enable export api ifx_bus_recv_buf for F3 receiving buffer
    2. add parameter function number (fn) to separate F2/F3 flow
    
    Signed-off-by: Owen Huang <Owen.Huang@infineon.com>

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/sdio.c
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/sdio.c
@@ -2111,7 +2111,7 @@ brcmf_sdio_read_control(struct brcmf_sdio *bus, u8 *hdr, uint len, uint doff)
 	}
 
 	/* Read remain of frame body */
-	sdret = brcmf_sdiod_recv_buf(bus->sdiodev, rbuf, rdlen);
+	sdret = brcmf_sdiod_recv_buf(bus->sdiodev, SDIO_FUNC_2, rbuf, rdlen);
 	bus->sdcnt.f2rxdata++;
 
 	/* Control frame failures need retransmission */
@@ -2197,7 +2197,7 @@ static uint brcmf_sdio_readframes(struct brcmf_sdio *bus, uint maxframes)
 		/* read header first for unknown frame length */
 		sdio_claim_host(bus->sdiodev->func1);
 		if (!rd->len) {
-			ret = brcmf_sdiod_recv_buf(bus->sdiodev,
+			ret = brcmf_sdiod_recv_buf(bus->sdiodev, SDIO_FUNC_2,
 						   bus->rxhdr, BRCMF_FIRSTREAD);
 			bus->sdcnt.f2rxhdrs++;
 			if (ret < 0) {
@@ -2254,7 +2254,7 @@ static uint brcmf_sdio_readframes(struct brcmf_sdio *bus, uint maxframes)
 		skb_pull(pkt, head_read);
 		pkt_align(pkt, rd->len_left, bus->head_align);
 
-		ret = brcmf_sdiod_recv_pkt(bus->sdiodev, pkt);
+		ret = brcmf_sdiod_recv_pkt(bus->sdiodev, SDIO_FUNC_2, pkt);
 		bus->sdcnt.f2rxdata++;
 		sdio_release_host(bus->sdiodev->func1);
 
