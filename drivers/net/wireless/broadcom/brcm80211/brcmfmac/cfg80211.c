91605ff870b626a040e4e4ee41d98987496beba9
Adesh Kumar Singh <adeshkumar.singh@infineon.com>
2023-03-27 23:44:40 -0500

    brcmfmac: Fix for wifi-generation info missing in wpa_cli status output
    Fix for wifi-generation info missing in wpa_cli status.
    
    Signed-off-by: Adesh Kumar Singh <adeshkumar.singh@infineon.com>

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/cfg80211.c
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/cfg80211.c
@@ -7731,6 +7731,7 @@ static s32 brcmf_get_assoc_ies(struct brcmf_cfg80211_info *cfg,
 
 	brcmf_clear_assoc_ies(cfg);
 
+	memset(cfg->extra_buf, '\0', WL_EXTRA_BUF_MAX);
 	err = brcmf_fil_iovar_data_get(ifp, "assoc_info",
 				       cfg->extra_buf, WL_ASSOC_INFO_MAX);
 	if (err) {
@@ -7748,6 +7749,7 @@ static s32 brcmf_get_assoc_ies(struct brcmf_cfg80211_info *cfg,
 		return -EINVAL;
 	}
 	if (req_len) {
+		memset(cfg->extra_buf, '\0', WL_EXTRA_BUF_MAX);
 		err = brcmf_fil_iovar_data_get(ifp, "assoc_req_ies",
 					       cfg->extra_buf,
 					       WL_ASSOC_INFO_MAX);
@@ -7776,6 +7778,7 @@ static s32 brcmf_get_assoc_ies(struct brcmf_cfg80211_info *cfg,
 	 * the assoc_resp_ie length should minus the 6 bytes which starts from rate_ie.
 	 */
 	if (resp_len) {
+		memset(cfg->extra_buf, '\0', WL_EXTRA_BUF_MAX);
 		err = brcmf_fil_iovar_data_get(ifp, "assoc_resp_ies",
 					       cfg->extra_buf,
 					       WL_ASSOC_INFO_MAX);
