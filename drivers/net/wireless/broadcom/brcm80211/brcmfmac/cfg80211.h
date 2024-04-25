e8b23b6dbf25e4a29a1f3ef7263857cb928bc735
Kshitij Bisht <kshitij.bisht@infineon.com>
2024-04-25 15:41:44 -0500

    non-upstream: vendor cmds string infrastructure with offload_config implementation
    IW Vendor Command String passing infrastructure is being added in this
    wherein fmac will handle user supplied strings directly via iw vendor
    interface. This can be used for migration of any wl cmd to iw wherein
    parameters passed to wl remain same is for iw.
    
    Example : wl -> iw
    
    1. wl offload_config Profile LowPwr 1 -s 0x3df
       echo 'offload_config Profile LowPwr 1 -s 0x3df ' | iw dev wlan0 vendor
       send 0x000319 0x1C -
    
    2. wl offload_config Enable 1
       echo 'offload_config Enable 1 ' | iw dev wlan0 vendor send 0x000319
       0x1C -
    
    Signed-off-by: Kshitij Bisht <kshitij.bisht@infineon.com>

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/cfg80211.h
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/cfg80211.h
@@ -528,4 +528,6 @@ void brcmf_cfg80211_free_netdev(struct net_device *ndev);
 int brcmf_set_wsec(struct brcmf_if *ifp, const u8 *key, u16 key_len, u16 flags);
 bool brcmf_is_apmode_operating(struct wiphy *wiphy);
 void brcmf_cfg80211_update_proto_addr_mode(struct wireless_dev *wdev);
+int ifx_vndr_cmdstr_hashtbl_init(void);
+void ifx_vndr_cmdstr_hashtbl_deinit(void);
 #endif /* BRCMFMAC_CFG80211_H */
