0017ca48aae9a690edaa1b9834cd5af7ac52297b
Shelley Yang <shelley.yang@infineon.com>
2025-02-17 23:36:34 -0600

    brcmfmac: adding Interworking IE in STA while scan from wpa_cli
    Dependency:
    1. Add interworking=1 in wpa_supplicant.conf
    2. Following commands should be given:
    wpa_cli vendor_elem_add 14 6b09000008000000000000
    
    Where wpa_cli commands expects:
    wpa_cli vendor_elem_add <frame type> <hex data>
    	where:
    	<frame type>: refer enum wpa_vendor_elem_frame in wpa_ctrl.h
    	<hex data>: IE data including the element type and length>
    
    Here 14 = probe request
    6b09000008000000000000 can be translated as:
    0x6b = 107 = Interworking IE type
    0x09 = IE length
    0x000008000000000000 = IE data
    3. wpa_cli scan
    
    Signed-off-by: ashish kumar <ashish4.kumar@infineon.com>
    Signed-off-by: Shelley Yang <shelley.yang@infineon.com>

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/cfg80211.h
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/cfg80211.h
@@ -14,6 +14,9 @@
 #include "p2p.h"
 #include "pno.h"
 
+/* Max length of Interworking element */
+#define BRCMF_IW_IES_MAX_BUF_LEN	8
+
 #define	DOT11_MGMT_HDR_LEN		24	/* d11 management header len */
 
 #define BRCMF_SCAN_IE_LEN_MAX		2048
