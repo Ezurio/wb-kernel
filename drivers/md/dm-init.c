c2a4cd14aa92dd1dd445fc39d3500523f2fa1f2d
Boris Krasnovskiy <boris.krasnovskiy@ezurio.com>
2025-04-12 09:11:35 -0400

    Revert "Fixed hang at boot with sd card"
    This reverts commit d06ea3d3b40cf69e30e7a9f3f3115eefa5b79e3b.

--- drivers/md/dm-init.c
+++ drivers/md/dm-init.c
@@ -298,7 +298,7 @@ static int __init dm_init_init(void)
 			dev_t dev;
 
 			DMINFO("waiting for device %s ...", waitfor[i]);
-			while (early_lookup_bdev(waitfor[i], &dev) || !driver_probe_done())
+			while (early_lookup_bdev(waitfor[i], &dev))
 				fsleep(5000);
 		}
 	}
