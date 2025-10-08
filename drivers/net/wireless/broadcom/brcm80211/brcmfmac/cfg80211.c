99565bebe9a11091393118046e040ba29f0dda4e
Carter Chen <carter.chen@infineon.com>
2025-10-08 04:13:41 -0500

    brcmfmac: unwrap brcmf_cfg80211_set_rekey_data from CONFIG_PM
    brcmf_cfg80211_set_rekey_data should not wrapped by CONFIG_PM
    since it is have become a common use from 6a5f232.
    
    Fixes SWLINUX-5463
    
    Signed-off-by: Carter Chen <carter.chen@infineon.com>

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/cfg80211.c
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/cfg80211.c
@@ -7059,7 +7059,6 @@ brcmf_cfg80211_update_conn_params(struct wiphy *wiphy,
 	return err;
 }
 
-#ifdef CONFIG_PM
 static int
 brcmf_cfg80211_set_rekey_data(struct wiphy *wiphy, struct net_device *ndev,
 			      struct cfg80211_gtk_rekey_data *gtk)
@@ -7084,7 +7083,6 @@ brcmf_cfg80211_set_rekey_data(struct wiphy *wiphy, struct net_device *ndev,
 
 	return ret;
 }
-#endif
 
 static int brcmf_cfg80211_set_pmk(struct wiphy *wiphy, struct net_device *dev,
 				  const struct cfg80211_pmk_conf *conf)
