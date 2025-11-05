43755573b4f0117a84e176e00c81c94114a46b95
Dean Ramsier <dean.ramsier@ezurio.com>
2025-11-05 12:23:03 -0500

    cc33xx: Add ble_enable to sysfs
    PROD-45891

--- drivers/net/wireless/ti/cc33xx/sysfs.c
+++ drivers/net/wireless/ti/cc33xx/sysfs.c
@@ -5,9 +5,56 @@
  * Copyright (C) 2013 Texas Instruments Inc.
  */
 
+#include <linux/device.h>
 #include "acx.h"
 #include "sysfs.h"
 
+static ssize_t ble_enable_show(struct device *dev,
+				  struct device_attribute *attr,
+				  char *buf)
+{
+	struct cc33xx *cc = dev_get_drvdata(dev);
+	ssize_t len;
+
+	mutex_lock(&cc->mutex);
+	len = sysfs_emit(buf, "%d\n", cc->ble_enable);
+	mutex_unlock(&cc->mutex);
+
+	return len;
+}
+
+static ssize_t ble_enable_store(struct device *dev,
+				   struct device_attribute *attr,
+				   const char *buf, size_t count)
+{
+	struct cc33xx *cc = dev_get_drvdata(dev);
+	int ret;
+	unsigned long value;
+
+	ret = kstrtoul(buf, 10, &value);
+	if (value != 1) {
+		cc33xx_warning("illegal value in ble_enable (only value allowed is 1)");
+		return -EINVAL;
+	}
+
+	if (value == cc->ble_enable) {
+		return count;
+	}
+
+	mutex_lock(&cc->mutex);
+
+	if (unlikely(cc->state != WLCORE_STATE_ON)) {
+		/* this will show up on "read" in case we are off */
+		cc->ble_enable = value;
+		goto out;
+	}
+
+	cc33xx_ble_enable(cc, value);
+out:
+	mutex_unlock(&cc->mutex);
+	return count;
+}
+static DEVICE_ATTR_RW(ble_enable);
 
 static ssize_t cc33xx_sysfs_read_fwlog(struct file *filp, struct kobject *kobj,
 				       struct bin_attribute *bin_attr,
@@ -50,6 +97,11 @@ int wlcore_sysfs_init(struct cc33xx *wl)
 {
 	int ret;
 
+	ret = device_create_file(wl->dev, &dev_attr_ble_enable);
+	if (ret < 0) {
+		cc33xx_error("failed to create sysfs file ble_enable");
+	}
+
 	/* Create sysfs file for the FW log */
 	ret = device_create_bin_file(wl->dev, &fwlog_attr);
 	if (ret < 0) {
@@ -62,4 +114,5 @@ int wlcore_sysfs_init(struct cc33xx *wl)
 void wlcore_sysfs_free(struct cc33xx *wl)
 {
 	device_remove_bin_file(wl->dev, &fwlog_attr);
+	device_remove_file(wl->dev, &dev_attr_ble_enable);
 }
