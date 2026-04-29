Infineon/ifx-wireless-drivers

non-upstream: btsdio: attach init/deinit
1. Attach init/deinit
2. Reduce bus sleep logs
3. Reject sleep only when bt is active in sdio bus sleep function

Fixes SWWLAN-145233

Signed-off-by: Owen Huang <Owen.Huang@infineon.com>

(cherry picked from commit be8c03153d518610a2b8e60646508ff780fbbac7)

drivers/net/wireless/broadcom/brcm80211/brcmfmac/sdio.c
@@ -40,6 +40,7 @@
 #include "bcdc.h"
 #include "fwil.h"
 #include "bt_shared_sdio.h"
+#include "bt_shared_sdio_ifx.h"
 #include "trxhdr.h"
 #include "feature.h"
 
@@ -1088,8 +1089,14 @@ static int brcmf_sdio_clkctl(struct brcmf_sdio *bus, uint target, bool pendok)
 			brcmf_sdio_wd_timer(bus, true);
 			break;
 		}
-
 #endif /* CONFIG_BRCMFMAC_BT_SHARED_SDIO */
+#ifdef CONFIG_IFX_BT_SHARED_SDIO
+		if (ifx_btsdio_is_active(bus->sdiodev->bus_if)) {
+			brcmf_dbg(SDIO, "BT is active, not switching to CLK_SDONLY\n");
+			brcmf_sdio_wd_timer(bus, true);
+			break;
+		}
+#endif /* CONFIG_IFX_BT_SHARED_SDIO */
 		/* Remove HT request, or bring up SD clock */
 		if (bus->clkstate == CLK_NONE)
 			brcmf_sdio_sdclk(bus, true);
@@ -1113,6 +1120,12 @@ static int brcmf_sdio_clkctl(struct brcmf_sdio *bus, uint target, bool pendok)
 			break;
 		}
 #endif /* CONFIG_BRCMFMAC_BT_SHARED_SDIO */
+#ifdef CONFIG_IFX_BT_SHARED_SDIO
+		if (ifx_btsdio_is_active(bus->sdiodev->bus_if)) {
+			brcmf_dbg(SDIO, "BT is active, not switching to CLK_NONE\n");
+			break;
+		}
+#endif /* CONFIG_IFX_BT_SHARED_SDIO */
 
 		/* Make sure to remove HT request */
 		if (bus->clkstate == CLK_AVAIL)
@@ -1134,10 +1147,6 @@ brcmf_sdio_bus_sleep(struct brcmf_sdio *bus, bool sleep, bool pendok)
 	int err = 0;
 	u8 clkcsr;
 
-	brcmf_dbg(SDIO, "Enter: request %s currently %s\n",
-		  (sleep ? "SLEEP" : "WAKE"),
-		  (bus->sleeping ? "SLEEP" : "WAKE"));
-
 #ifdef CONFIG_BRCMFMAC_BT_SHARED_SDIO
 	/* The following is the assumption based on which the hook is placed.
 	 * From WLAN driver, either from the active contexts OR from the
@@ -1154,12 +1163,21 @@ brcmf_sdio_bus_sleep(struct brcmf_sdio *bus, bool sleep, bool pendok)
 	 * clock and if another WLAN context is active they are any way
 	 * serialized with sdlock.
 	 */
-	if (brcmf_btsdio_bus_count(bus->sdiodev->bus_if)) {
+	if (sleep && brcmf_btsdio_bus_count(bus->sdiodev->bus_if)) {
 		brcmf_dbg(SDIO, "Cannot sleep when BT is active\n");
-		err = -EBUSY;
-		goto done;
+		return -EBUSY;
 	}
 #endif /* CONFIG_BRCMFMAC_BT_SHARED_SDIO */
+#ifdef CONFIG_IFX_BT_SHARED_SDIO
+	if (sleep && ifx_btsdio_is_active(bus->sdiodev->bus_if)) {
+		brcmf_dbg(SDIO, "Bus cannot sleep when BT is active\n");
+		return -EBUSY;
+	}
+#endif /* CONFIG_IFX_BT_SHARED_SDIO */
+
+	brcmf_dbg(SDIO, "Enter: request %s currently %s\n",
+		  (sleep ? "SLEEP" : "WAKE"),
+		  (bus->sleeping ? "SLEEP" : "WAKE"));
 
 	/* If SR is enabled control bus state with KSO */
 	if (bus->sr_enabled) {
@@ -4324,7 +4342,7 @@ static void brcmf_sdio_bus_watchdog(struct brcmf_sdio *bus)
 #endif				/* DEBUG */
 
 	/* On idle timeout clear activity flag and/or turn off clock */
-	if (!bus->dpc_triggered &&
+	if (!bus->dpc_triggered && !ifx_btsdio_is_active(bus->sdiodev->bus_if) &&
 	    brcmf_btsdio_bus_count(bus->sdiodev->bus_if) == 0) {
 		rmb();
 		if ((!bus->dpc_running) && (bus->idletime > 0) &&
@@ -5231,6 +5249,8 @@ static void brcmf_sdio_firmware_callback(struct device *dev, int err,
 			goto free;
 		}
 
+		ifx_btsdio_init(bus_if);
+
 		/* Register for ULP events */
 		if (sdiod->func1->device == SDIO_DEVICE_ID_BROADCOM_CYPRESS_43012 ||
 		    sdiod->func1->device == SDIO_DEVICE_ID_BROADCOM_CYPRESS_43022 ||
@@ -5437,6 +5457,8 @@ fail:
 /* Detach and free everything */
 void brcmf_sdio_remove(struct brcmf_sdio *bus)
 {
+	struct brcmf_bus *bus_if = bus->sdiodev->bus_if;
+
 	brcmf_dbg(TRACE, "Enter\n");
 
 	if (bus) {
@@ -5525,8 +5547,10 @@ void brcmf_sdio_remove(struct brcmf_sdio *bus)
 		if (bus->sdiodev->settings)
 			brcmf_release_module_param(bus->sdiodev->settings);
 #ifdef CONFIG_BRCMFMAC_BT_SHARED_SDIO
-		brcmf_btsdio_detach(bus->sdiodev->bus_if);
+		brcmf_btsdio_detach(bus_if);
 #endif /* CONFIG_BRCMFMAC_BT_SHARED_SDIO */
+
+		ifx_btsdio_deinit(bus_if);
 
 		kfree(bus->rxbuf);
 		kfree(bus->hdrbuf);

Ezurio/wb-kernel

drivers/net/wireless/broadcom/brcm80211/brcmfmac/sdio.c
@@ -40,6 +40,7 @@
 #include "bcdc.h"
 #include "fwil.h"
 #include "bt_shared_sdio.h"
+#include "bt_shared_sdio_ifx.h"
 #include "trxhdr.h"
 #include "feature.h"
 
@@ -1090,8 +1091,14 @@ static int brcmf_sdio_clkctl(struct brcmf_sdio *bus, uint target, bool pendok)
 			brcmf_sdio_wd_timer(bus, true);
 			break;
 		}
-
 #endif /* CONFIG_BRCMFMAC_BT_SHARED_SDIO */
+#ifdef CONFIG_INFFMAC_BT_SHARED_SDIO
+		if (inf_btsdio_is_active(bus->sdiodev->bus_if)) {
+			brcmf_dbg(SDIO, "BT is active, not switching to CLK_SDONLY\n");
+			brcmf_sdio_wd_timer(bus, true);
+			break;
+		}
+#endif /* CONFIG_INFFMAC_BT_SHARED_SDIO */
 		/* Remove HT request, or bring up SD clock */
 		if (bus->clkstate == CLK_NONE)
 			brcmf_sdio_sdclk(bus, true);
@@ -1115,6 +1122,12 @@ static int brcmf_sdio_clkctl(struct brcmf_sdio *bus, uint target, bool pendok)
 			break;
 		}
 #endif /* CONFIG_BRCMFMAC_BT_SHARED_SDIO */
+#ifdef CONFIG_INFFMAC_BT_SHARED_SDIO
+		if (inf_btsdio_is_active(bus->sdiodev->bus_if)) {
+			brcmf_dbg(SDIO, "BT is active, not switching to CLK_NONE\n");
+			break;
+		}
+#endif /* CONFIG_INFFMAC_BT_SHARED_SDIO */
 
 		/* Make sure to remove HT request */
 		if (bus->clkstate == CLK_AVAIL)
@@ -1136,10 +1149,6 @@ brcmf_sdio_bus_sleep(struct brcmf_sdio *bus, bool sleep, bool pendok)
 	int err = 0;
 	u8 clkcsr;
 
-	brcmf_dbg(SDIO, "Enter: request %s currently %s\n",
-		  (sleep ? "SLEEP" : "WAKE"),
-		  (bus->sleeping ? "SLEEP" : "WAKE"));
-
 #ifdef CONFIG_BRCMFMAC_BT_SHARED_SDIO
 	/* The following is the assumption based on which the hook is placed.
 	 * From WLAN driver, either from the active contexts OR from the
@@ -1156,12 +1165,21 @@ brcmf_sdio_bus_sleep(struct brcmf_sdio *bus, bool sleep, bool pendok)
 	 * clock and if another WLAN context is active they are any way
 	 * serialized with sdlock.
 	 */
-	if (brcmf_btsdio_bus_count(bus->sdiodev->bus_if)) {
+	if (sleep && brcmf_btsdio_bus_count(bus->sdiodev->bus_if)) {
 		brcmf_dbg(SDIO, "Cannot sleep when BT is active\n");
-		err = -EBUSY;
-		goto done;
+		return -EBUSY;
 	}
 #endif /* CONFIG_BRCMFMAC_BT_SHARED_SDIO */
+#ifdef CONFIG_INFFMAC_BT_SHARED_SDIO
+	if (sleep && inf_btsdio_is_active(bus->sdiodev->bus_if)) {
+		brcmf_dbg(SDIO, "Bus cannot sleep when BT is active\n");
+		return -EBUSY;
+	}
+#endif /* CONFIG_INFFMAC_BT_SHARED_SDIO */
+
+	brcmf_dbg(SDIO, "Enter: request %s currently %s\n",
+		  (sleep ? "SLEEP" : "WAKE"),
+		  (bus->sleeping ? "SLEEP" : "WAKE"));
 
 	/* If SR is enabled control bus state with KSO */
 	if (bus->sr_enabled) {
@@ -4327,7 +4345,7 @@ static void brcmf_sdio_bus_watchdog(struct brcmf_sdio *bus)
 #endif				/* DEBUG */
 
 	/* On idle timeout clear activity flag and/or turn off clock */
-	if (!bus->dpc_triggered &&
+	if (!bus->dpc_triggered && !inf_btsdio_is_active(bus->sdiodev->bus_if) &&
 	    brcmf_btsdio_bus_count(bus->sdiodev->bus_if) == 0) {
 		rmb();
 		if ((!bus->dpc_running) && (bus->idletime > 0) &&
@@ -5250,6 +5268,8 @@ static void brcmf_sdio_firmware_callback(struct device *dev, int err,
 			goto free;
 		}
 
+		inf_btsdio_init(bus_if);
+
 		/* Register for ULP events */
 		if (sdiod->func1->device == SDIO_DEVICE_ID_BROADCOM_CYPRESS_43012 ||
 		    sdiod->func1->device == SDIO_DEVICE_ID_BROADCOM_CYPRESS_43022 ||
@@ -5464,6 +5484,9 @@ fail:
 /* Detach and free everything */
 void brcmf_sdio_remove(struct brcmf_sdio *bus)
 {
+#ifdef CONFIG_BRCMFMAC_BT_SHARED_SDIO
+	struct brcmf_bus *bus_if = bus->sdiodev->bus_if;
+#endif
 	brcmf_dbg(TRACE, "Enter\n");
 
 	if (bus) {
@@ -5552,11 +5575,13 @@ void brcmf_sdio_remove(struct brcmf_sdio *bus)
 		if (bus->sdiodev->settings)
 			brcmf_release_module_param(bus->sdiodev->settings);
 #ifdef CONFIG_BRCMFMAC_BT_SHARED_SDIO
-		brcmf_btsdio_detach(bus->sdiodev->bus_if);
+		brcmf_btsdio_detach(bus_if);
 #endif /* CONFIG_BRCMFMAC_BT_SHARED_SDIO */
 
 		release_firmware(bus->sdiodev->clm_fw);
 		bus->sdiodev->clm_fw = NULL;
+		inf_btsdio_deinit(bus_if);
+
 		kfree(bus->rxbuf);
 		kfree(bus->hdrbuf);
 		kfree(bus);